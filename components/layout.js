d2d68b8a4098816ede9efdaa8ba1cfae3fd1ba41
Aryanmehta21 <aryanmehta310@gmail.com>
2023-03-13 16:18:58 +0530

    13-03-2023

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Footer from './Footer';
 const layout = ({children}) => {
   return (
-    <div className="lg:max-w-screen-2xl p-10 md:max-w-2xl md:mx-auto font-poppins bg-gradient-to-b from-black text-black ">
+    <div className="lg:max-w-screen-2xl p-10 md:max-w-2xl md:mx-auto font-poppins bg-black text-black ">
         <Nav/>
         <main>{children}</main>
         <Footer />
