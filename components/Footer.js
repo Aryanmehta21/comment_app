cb41310f0553745b456f05374115c83899ebbdf7
Aryanmehta21 <aryanmehta310@gmail.com>
2023-03-13 16:07:25 +0530

    copyright

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-36 position-absolute'>
+        <div className='text-lg text-white flex items-center justify-center' >
+        <p className='text-lg'>Copyright <sup>&copy;</sup> <span className='text-red-700 font-black'>Aryan Mehta</span> 2023</p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,12 @@
 import Nav from './Nav';
 import React from 'react'
+import Footer from './Footer';
 const layout = ({children}) => {
   return (
     <div className="lg:max-w-screen-2xl p-10 md:max-w-2xl md:mx-auto font-poppins bg-gradient-to-b from-black text-black ">
         <Nav/>
         <main>{children}</main>
+        <Footer />
     </div>
   )
 }
